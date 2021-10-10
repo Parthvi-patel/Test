ce0e9806a6da35865c5fda3efa28fe7142cbd16d
Parthvi-patel <78634250+Parthvi-patel@users.noreply.github.com>
2021-10-10 13:01:45 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner s1=new Scanner(System.in);
+        System.out.println("Enter the value of A & b");
+        int a = s1.nextInt();
+        int b = s1.nextInt();
+      //submisstion of two numbers
+        int c= a+b;
+        System.out.println("submission="+c);
+    }
+
+
+}
